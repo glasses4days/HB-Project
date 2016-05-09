9159117d213decaea10d907bf046919f39c70ac9
glasses4days <cooper@Coopers-MacBook-Air.local>
2016-05-09 11:24:09 -0700

    Setup skeleton for server.py and  model.py

--- model.py
+++ model.py
@@ -1,1 +1,27 @@
-model
+"""Models and database functions for Bark Park"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+##############################################################################
+# Model definitions
+
+##############################################################################
+#Helper functions
+
+# def connect_to_db(app):
+#     """Connect the database to Flask app."""
+
+#     # Configure to use PstgreSQL database
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///'
+#     db.app = app
+#     db.init_app(app)
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
--- server.py
+++ server.py
@@ -1,1 +1,29 @@
-server
+""" Bark Park server """
+
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+#from model import
+
+app = Flask(__name__)
+
+app.secret_key = "very_very_secret"
+
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage"""
+
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    app.debug = True
+
+    #connect_to_db(app)
+
+    DebugToolbarExtension(app)
+
+    app.run()
