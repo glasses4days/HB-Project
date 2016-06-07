7f670f5a712a3b4ad2f31d6bb21b5e6b1f216ca8
glasses4days <cooper@Coopers-MacBook-Air.local>
2016-06-07 16:05:34 -0700

    Added tests, up to 91% coverage

--- tests.py
+++ tests.py
@@ -6,7 +6,6 @@ from server import app
 import server
 
 
-
 class FlaskTestsBasic(unittest.TestCase):
     """Tests for barkPark"""
 
@@ -15,11 +14,33 @@ class FlaskTestsBasic(unittest.TestCase):
         self.client = server.app.test_client()
         # Show Flask errors that happen during tests
         server.app.config['TESTING'] = True
+        
 
-    def test_homepage(self):
+    def test_homepage_render(self):
         #Checks that the h1 is rendering in homepage.html
         result = self.client.get("/")
-        self.assertIn("<h1>Bark Park!!</h1>", result.data)
+        self.assertIn("<h1 class=\"title\">Bark Park!</h1>", result.data)
+
+    def test_signup_form_render(self):
+        #Checks that signup form is rendering properly
+        result = self.client.get("/signup_form")
+        self.assertIn("<p>Please enter a user name and password</p>", result.data)
+
+    def test_signin_form_render(self):
+        #Checks that signup form is rendering properly
+        result = self.client.get("/sign_in_form")
+        self.assertIn("<p>Please sign in</p>", result.data)
+
+    def test_enter_info_render(self):
+        #Checks that signup form is rendering properly
+        result = self.client.get("/enter_info")
+        self.assertIn("Park id:", result.data)
+
+    def test_logout_redirect(self):
+        """Tests logout redircts back to homepage"""
+
+        result = self.client.get("/")
+        self.assertIn("<h1 class=\"title\">Bark Park!</h1>", result.data)
 
 
 class FlaskTestsDatabase(TestCase):
@@ -31,6 +52,11 @@ class FlaskTestsDatabase(TestCase):
         # Get the Flask test client
         self.client = app.test_client()
         app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'key'
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user'] = 25
 
         # Connect to test database
         connect_to_db(app, "postgresql:///testdb")
@@ -50,6 +76,51 @@ class FlaskTestsDatabase(TestCase):
 
         result = self.client.get("/create_map_features.json")
         self.assertIn("JAPANTOWN PEACE PLAZA", result.data)
+
+    def test_park_info(self):
+        """Test park_info render correctly"""
+        # Checks that park info is rendering correctly
+        result = self.client.get("/park_info/2", follow_redirects=True)
+        self.assertIn("JAPANTOWN PEACE PLAZA", result.data)
+
+    def test_add_new_user(self):
+        """Test adding new user"""
+
+        result = self.client.post("/add_new_user",
+                                  data={"user_name": "rachel", "password": "123", "email": "rachel@rachel.com"},
+                                  follow_redirects=True)
+        self.assertIn("<p>Please sign in</p>", result.data)
+
+    def test_sign_in(self):
+        """Test sign_in"""
+
+        result = self.client.post("/sign_in",
+                                  data={"email": "user@user.com", "password": "password"},
+                                  follow_redirects=True)
+        self.assertIn("<h1 class=\"title\">Bark Park!</h1>", result.data)
+
+    def test_logout(self):
+        """Tests logout works and redirects back to homepage"""
+
+        result = self.client.get("logout", follow_redirects=True)
+        self.assertIn("<h1 class=\"title\">Bark Park!</h1>", result.data)
+
+    def test_add_new_comment(self):
+        """Test adding new comment"""
+
+        result = self.client.post("/add_new_comment/2",
+                                  data={"user_id": 25, "park_id": "2", "content": "My dog loves this park!"},
+                                  follow_redirects=True)
+        self.assertIn("<h1 class=\"title\">Bark Park!</h1>", result.data)
+
+    def test_add_new_photo(self):
+        """Test adding new photo"""
+
+        result = self.client.post("/add_new_photo/2",
+                                  data={"park_id": "2", "user_id": 25, "description": "Taurus having a great time!", "photo": "http://s3-media3.fl.yelpcdn.com/bphoto/ZUhteyc0UWdWbgM-4VbZhw/o.jpg"},
+                                  follow_redirects=True)
+        self.assertIn("<h1 class=\"title\">Bark Park!</h1>", result.data)
+
 
 if __name__ == "__main__":
     unittest.main()
